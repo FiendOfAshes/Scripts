134041ac482bba61c9818be263fa0420ba58d06c
Aisha Atif <143438686+FiendOfAshes@users.noreply.github.com>
2025-03-20 02:52:13 +0400

    Update palette_generator.py
    Added comments

--- palette_generator.py
+++ palette_generator.py
@@ -1,4 +1,4 @@
-def palette_generator(colours=['#139FBE', '#054655', '#0E788F'], size=(8, 1), with_saturation={}): # with_saturation allows the specifying of saturation values for specific colours
+def palette_generator(colours=['#139FBE', '#054655', '#0E788F'], size=(8, 1), with_saturation={}):
     try:
         from matplotlib import pyplot as plt 
         from matplotlib import patches 
@@ -12,7 +12,9 @@ def palette_generator(colours=['#139FBE', '#054655', '#0E788F'], size=(8, 1), wi
     fig, ax = plt.subplots(figsize=size) 
     for i, colour in enumerate(colours):
         if colour in with_saturation.keys():
-                ax.add_patch(patches.Rectangle((i, 0), 1, 1, color=colour, alpha=with_saturation.get(colour)))
+                ax.add_patch(patches.Rectangle((i, 0), 1, 1, color=colour, alpha=with_saturation.get(colour))) # the first value defines the x, y position of the Rectangle,
+            # the second and third value is the width and height of the Rectange but changing them has no effect on the output figure
+            # alpha value controls the saturation, by default it is 1 and ranges can be passed from 0-1
         else:
             ax.add_patch(patches.Rectangle((i, 0), 1, 1, color=colour))
     
